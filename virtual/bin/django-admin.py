17ff4730ce750b74a073bb7841796a002f4af2a9
virginiah894 <virgyperry@gmail.com>
2020-01-18 16:13:24 +0300

    system updates

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/MORINGA PROJECTS/Neighborhood/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
