37fa5e2ec4ad5132c691f9c2976dfacc65e4f3a2
virginiah894 <virgyperry@gmail.com>
2020-01-19 17:43:41 +0300

    Allows admin to register all models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile,Hood,updates,Businesses,Medicalservices,HealthServices,Adminstration,Post,Comments
+
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(HealthServices)
+admin.site.register(Hood)
+admin.site.register(Post)
+admin.site.register(Comments)
+
+admin.site.register(updates)
+admin.site.register(Businesses)
+admin.site.register(Medicalservices)
+admin.site.register(Adminstration)
