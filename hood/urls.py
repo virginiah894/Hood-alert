e3f22979ec2cfc23e72ba89e08afb6dfb909fce0
virginiah894 <virgyperry@gmail.com>
2020-01-19 17:45:23 +0300

    Adds the routes to be displayed

--- hood/urls.py
+++ hood/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path , include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 
     
     path('', views.home,name='home'),
+    path('profile/', views.profile , name = 'profile'),
+    path('update_profile/',views.update_profile,name='update'),
+    path('updates/', views.updates, name='updates'),
+    path('new/update', views.new_update, name = 'newUpdate'),
+
+
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
